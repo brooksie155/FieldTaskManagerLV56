faef5522313172b9fd20ce8f6e7fb827fde8af67
Stephen Brooks <stephen.brooks@reporo.com>
2018-04-19 10:18:18 +0100

    migrate routes

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-
 /**
  * Use for route construction ?? WIP
  */
@@ -24,62 +23,10 @@ $routing = [
     ]
 ];
 
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/client/{id}', [
-    'as' => 'client_get',
-    'uses' => 'API\ClientManager@getAction'
-]);
+include('api/client.php');
 
-Route::get('/clients/listAction', [
-    'as' => 'client_list',
-    'uses' => 'API\ClientManager@listAction'
-]);
-
-Route::get('/clients/search', [
-    'as' => 'client_search',
-    'uses' => 'API\ClientManager@searchAction'
-]);
-
-/*
- * curl -X POST -d "company=test1&firstname=Bob&lastname=jim&email=bob@bob.bob" -H "X-Requested-With: XMLHttpRequest" http://thefield-tm.test/api/clients/add
- */
-Route::post('/clients/add', [                
-    'as' => 'client_add',
-    'uses' => 'API\ClientManager@addAction'    
-]);
-
-Route::post('/clients/update/{id}', [                
-    'as' => 'client_update',
-    'uses' => 'API\ClientManager@updateAction'    
-]);
-
-Route::post('/clients/delete/{id}', [                
-    'as' => 'client_delete',
-    'uses' => 'API\ClientManager@deleteAction'    
-]);
-
-Route::get('/clients/deleted', [               
-    'as' => 'client_deleted',
-    'uses' => 'API\ClientManager@listDeletedAction'    
-]);
-
-Route::post('/clients/restore/{id}', [               
-    'as' => 'client_restore',
-    'uses' => 'API\ClientManager@listDeletedAction'    
-]);
-
-Route::post('/clients/expunge/{id}', [               
-    'as' => 'client_expunge',
-    'uses' => 'API\ClientManager@listDeletedAction'    
-]);
-
-
-
-
-// will have similar routes for other entities .
-//  ... can theny be generated dynamically?
 
