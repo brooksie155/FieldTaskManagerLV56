c31b8c8bc0f16b6ca3fdab48c260d9e811f420c2
Stephen Brooks <stephen.brooks@reporo.com>
2018-04-25 09:07:00 +0100

    update forms to questions table update respondents, record to be created per project

--- database/migrations/2018_04_11_091851_respondents.php
+++ database/migrations/2018_04_11_091851_respondents.php
@@ -9,6 +9,14 @@ class Respondents extends Migration
     /**
      * Run the migrations.
      *
+     * Link respondent to project, profile may vary between projects (meh ?)
+     * - does respondent require different login per project which may run concurrently?
+     * - or should we try to use same login (email will probably be same) in which case 
+     * can we merge profiles, or use an intermediary table to hold profiles on per respondent/project basis.
+     * 
+     * --- assume respondent data will be removed once project complete...
+     * 
+     * 
      * @return void
      */
     public function up()
@@ -26,11 +34,14 @@ class Respondents extends Migration
             $table->string('gender_other');
             $table->text('profile');  // data from screener form
             $table->integer('recruiter_id');
-            
+            $table->integer('project_id');   // keep different record per project (even if respondent is reused)
+            $table->enum('duplicate_flag',['Y','N']); // if possible duplicate detected on current or recent projects (90 days)
+            $table->integer('duplicate_respondent_id')->default(0);
             $table->softDeletes();
             $table->rememberToken();
             $table->timestamps();
             $table->index('recruiter_id');
+            $table->index(['project_id','recruiter_id']);  
         });
     }
 
